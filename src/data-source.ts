4d7fa761692ae31acab68735179d4d24b545b7c8
karthikeyanrb1983 <karthikeyasoft@gmail.com>
2025-05-21 14:37:27 +0530

    chatbot

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,6 +3,7 @@
 import { DataSource } from "typeorm";
 import { User } from "./entities/User";
 import { Task } from "./entities/Task";
+import { Chat } from "./entities/Chat";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,7 +15,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User, Task],
+  entities: [User, Task, Chat],
   synchronize: true,
   logging: false,
 });
